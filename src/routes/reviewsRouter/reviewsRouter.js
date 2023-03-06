2fd7315957b033233b6b5959218fc6db8ae82b83
Franandrade94 <franandrade15994@gmail.com>
2023-03-06 16:36:58 -0300

    Reviews

--- src/routes/reviewsRouter/reviewsRouter.js
+++ src/routes/reviewsRouter/reviewsRouter.js
@@ -1,5 +1,6 @@
 const Router = require('express');
 const { postReviews } = require('../../controllers/reviews/postReviews');
+const { getReviews } = require('../../controllers/reviews/getReviews');
 const { reviewsVerified } = require('../../controllers/reviews/reviewsVerified');
 
 const reviewsRouter = Router();
