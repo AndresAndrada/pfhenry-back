9bceabe83d23486d487fbafb20c65461972f8747
AndresAndrada <andresandrada1994@gmail.com>
2023-03-17 23:25:43 -0300

    Pobando depoloy de Render con findAll en getProduct

--- src/controllers/products/getProducts.js
+++ src/controllers/products/getProducts.js
@@ -11,63 +11,72 @@ ORDER_BY = {
 }
 
 const getProduct = async (req, res) => {
-    const { name, orderby, types, pricerange } = req.query;
-    const myOrderBy = (orderby)?ORDER_BY[orderby]: ORDER_BY['default']
+    try {
+        const allProducts =  await Product.findAll();
+        res.send(allProducts);
+    } catch (error) {
+        res.send({ message: error });
+    }
+}
 
-    let whereName = (name)? {
-        [Op.and]:[
-        Sequelize.where(
-            Sequelize.fn('lower', Sequelize.col('name')),
-            {
-              [Op.like]: name.toLowerCase()
-            }
-          ),
-      ]
-    }:null
+// const getProduct = async (req, res) => {
+//     const { name, orderby, types, pricerange } = req.query;
+//     const myOrderBy = (orderby)?ORDER_BY[orderby]: ORDER_BY['default']
 
-    const wherePriceRange = (pricerange)?{ 
-        price: {
-            [Op.between]: pricerange.split(',')
-        }
-    }:{}
+//     let whereName = (name)? {
+//         [Op.and]:[
+//         Sequelize.where(
+//             Sequelize.fn('lower', Sequelize.col('name')),
+//             {
+//               [Op.like]: name.toLowerCase()
+//             }
+//           ),
+//       ]
+//     }:null
 
-        if(whereName == null){
-            whereName = {
-                [Op.and]: [
-                    wherePriceRange 
-                ]
-            }
-        }else{
-            whereName[Op.and].push(wherePriceRange)
-        }
+//     const wherePriceRange = (pricerange)?{ 
+//         price: {
+//             [Op.between]: pricerange.split(',')
+//         }
+//     }:{}
+
+//         if(whereName == null){
+//             whereName = {
+//                 [Op.and]: [
+//                     wherePriceRange 
+//                 ]
+//             }
+//         }else{
+//             whereName[Op.and].push(wherePriceRange)
+//         }
     
 
 
 
-    const whereType = (types)?{
-        id:{
-          [Op.in]: types.split(',')
-        }
-      }:{}
+//     const whereType = (types)?{
+//         id:{
+//           [Op.in]: types.split(',')
+//         }
+//       }:{}
 
 
-    try {
-            const products = await Product.findAll({
-                include: [{
-                    model: Types,
-                    where: whereType
-                }],
-                order: [
-                    myOrderBy,
-                ],
-                where: whereName
-              });
-            if(!products.length) return res.send([]);
-            res.send(products);
+//     try {
+//             const products = await Product.findAll({
+//                 include: [{
+//                     model: Types,
+//                     where: whereType
+//                 }],
+//                 order: [
+//                     myOrderBy,
+//                 ],
+//                 where: whereName
+//               });
+//             if(!products.length) return res.send([]);
+//             res.send(products);
       
-    } catch (error) {
-        res.send({ message: error.message });
-    };
-};
+//     } catch (error) {
+//         res.send({ message: error.message });
+//     };
+// };
 
 module.exports = { getProduct };
